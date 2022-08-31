580add81666bc0f9ea2dabcc7845896deaaf5f59
Moazam05 <salmanmoazam08@gmail.com>
2022-08-31 17:23:16 +0500

    folder structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Cart from './Pages/Cart';
+import Home from './Pages/Home';
+import NotFound from './Pages/NotFound';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/cart' element={<Cart />} />
+      {/* 404 Page */}
+      <Route path='*' element={<NotFound />} />
+    </Routes>
   );
-}
+};
 
 export default App;
