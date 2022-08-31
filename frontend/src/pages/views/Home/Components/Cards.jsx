86554ac59df159bf771d6aae101256405296a564
Moazam05 <salmanmoazam08@gmail.com>
2022-08-31 18:03:22 +0500

    Card Page Design

---
+++ frontend/src/pages/views/Home/Components/Cards.jsx
@@ -0,0 +1,49 @@
+// React Imports
+import React from 'react';
+// import { Link } from 'react-router-dom';
+// CSS Imports
+import './Cards.css';
+
+const Cards = ({ product }) => {
+  return (
+    <div
+      className='card mx-2'
+      style={{ width: 'unset', padding: '2rem', minHeight: '400px' }}
+    >
+      <div className='d-flex flex-column align-items-center px-5 remove-px'>
+        <div className='card-image-wrapper'>
+          <img
+            className='mx-auto img-thumbnail mt-3'
+            src='https://www.apple.com/v/iphone-13-pro/a/images/overview/design/finishes_1_sierra_blue__2bovafkl4yaa_large.jpg'
+            alt='logo'
+          />
+        </div>
+      </div>
+      <div className='card-body text-center mx-auto d-flex flex-column justify-content-center'>
+        <h5
+          className='card-title font-weight-bold'
+          style={{ minHeight: '50px' }}
+        >
+          {/* {product?.title} */}
+          iphone 13
+        </h5>
+        <p className='card-text'>
+          $1200
+          {/* {product?.price} */}
+        </p>
+        <div className='button-fixed'>
+          <br />
+          <button
+            type='button'
+            class='btn btn-warning'
+            // onClick={() => addToCart(product)}
+          >
+            ADD TO CART
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
--- frontend/src/pages/views/Home/index.js
+++ frontend/src/pages/views/Home/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import Layout from '../../../components/Layout';
+import Cards from './Components/Cards';
 
 const Home = () => {
-  return <Layout>Home</Layout>;
+  return (
+    <Layout>
+      <div className='container'>
+        <div className='row mt-5'>
+          <div className='col-md-6 col-lg-4'>
+            <Cards />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Home;
