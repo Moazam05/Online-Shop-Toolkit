176e3441b21ecb848cb884de5806a1bd4e6912e2
Moazam05 <salmanmoazam08@gmail.com>
2022-09-02 00:59:51 +0500

    localStorage

--- frontend/src/pages/views/Cart/Store/index.js
+++ frontend/src/pages/views/Cart/Store/index.js
@@ -1,10 +1,13 @@
 // ** Redux Imports
 import { createSlice } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
 
 export const cartSlice = createSlice({
   name: 'cart',
   initialState: {
-    cartItems: [],
+    cartItems: localStorage.getItem('cartItems')
+      ? JSON.parse(localStorage.getItem('cartItems'))
+      : [],
     cartTotalQuantity: 0,
     cartTotalAmount: 0,
   },
@@ -17,10 +20,27 @@ export const cartSlice = createSlice({
       // If Product found in cart then only increase quantity
       if (itemIndex >= 0) {
         state.cartItems[itemIndex].cartQuantity += 1;
+        toast.info(
+          `${state.cartItems[itemIndex].title.substring(
+            0,
+            11
+          )} increase quantity`,
+          {
+            position: 'top-center',
+          }
+        );
       } else {
         const tempProduct = { ...action.payload, cartQuantity: 1 };
         state.cartItems.push(tempProduct);
+        toast.success(
+          `${action.payload.title.substring(0, 11)} Added To Cart`,
+          {
+            position: 'top-center',
+          }
+        );
       }
+
+      localStorage.setItem('cartItems', JSON.stringify(state.cartItems));
     },
   },
 });
